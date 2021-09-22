baa3e1c6f761a6062caf7c92bd6fa8432b814818
Thiago Soares <thiagodevps@gmail.com>
2021-09-22 17:19:52 -0300

    feature

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document{
           <link rel="shortcut icon" href="/logo-nobg.png" type="image/png"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
